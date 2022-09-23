74291b2f973948f97531bce1ce50ff66d0868ef8
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-09-23 16:41:32 -0300

    feat: implementation footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,27 @@
-﻿import styles from './styles.module.scss';
+﻿import {ImLocation2, ImPhone} from 'react-icons/im';
+import styles from './styles.module.scss';
 
 export function Footer(){
     return(
         <footer className={styles.footerConteiner}>
             <div className={styles.footerContent}>
-                <span>Institucional</span>
-                <a>Conheça o SENAI</a>
-                <a>Diretoria</a>
-                <a>Autonomia Regional</a>
+                <div>
+                    <span> 
+                        <ImLocation2 />
+                        Rua de Pedestre SE
+                    </span>
+                    <p>Av. Paulista</p>
+                </div>
+                <div>
+                    <span>
+                        <ImPhone />
+                        Central de Atendimento
+                    </span>
+                    <p>(11)088-055-1000</p>
+                </div>
+            </div>
+            <div className={styles.footerCopyright}>
+                <small>Copyright 2022 © Todos os direitos reservados.</small>
             </div>
         </footer>
     )
