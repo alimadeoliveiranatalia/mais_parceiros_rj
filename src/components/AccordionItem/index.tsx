21a8ae323d08a56b8c6daa73433a3d2f7f5c59c3
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-09-28 16:05:48 -0300

    refactor: implements tag <details>

--- src/components/AccordionItem/index.tsx
+++ src/components/AccordionItem/index.tsx
@@ -2,30 +2,17 @@
 
 import styles from './styles.module.scss';
 
-export function AccordionItem({
-    showDescription,
-    ariaExpanded,
-    fontWeightBold,
-    item,
-    index,
-    onClick
-    }){
-        function showDescription(){
-            const div = document.querySelector('';)
-            if(div)
-        }
-        return (
-            <div className={styles.accordionItem} key={item.question}>
-                <dt>
-                    <button
-                        onClick={ onClick }
-                    >{item.question}<AiOutlineDown /><AiOutlineUp /></button>
-                </dt>
-                <details>
-                    <p
-                        id={index + 1}
-                    >{item.answer}</p>
-                </details>
-            </div>
-        )
+export function AccordionItem({ item, index}){
+    
+    return (
+        <div className={styles.accordionItem} key={item.question}>
+            <details>
+                <summary>{item.question}{ true ? <AiOutlineDown /> : <AiOutlineUp />}              
+                </summary>
+                <p
+                    id={index + 1}
+                >{item.answer}</p>
+            </details>
+        </div>
+    )
 }
