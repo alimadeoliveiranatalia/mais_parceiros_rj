3d145b4eaa43fb4ec03799204843ae2e45e4b091
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-09-28 16:30:41 -0300

    feat: finished accordion

--- src/components/AccordionItem/index.tsx
+++ src/components/AccordionItem/index.tsx
@@ -1,13 +1,17 @@
-﻿import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
+﻿import { useState } from "react";
+import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
 
 import styles from './styles.module.scss';
 
 export function AccordionItem({ item, index}){
-    
+    const [ open, setOpen ] = useState(false);
+    const onToggle = () => {
+        setOpen(!open);
+    }
     return (
         <div className={styles.accordionItem} key={item.question}>
             <details>
-                <summary>{item.question}{ true ? <AiOutlineDown /> : <AiOutlineUp />}              
+                <summary onClick={onToggle}>{item.question}{ open ? <AiOutlineUp /> : <AiOutlineDown />}              
                 </summary>
                 <p
                     id={index + 1}
