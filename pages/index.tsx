88838f849c4d5345854e2ec26fb33290b3a9b596
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-09-30 13:12:39 -0300

    feat: reposive video

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { Accordion } from '../src/components/Accordion';
 import { Footer } from '../src/components/Footer';
+import { LinkYoutube } from '../src/components/LinkYoutube';
 import styles from '../styles/Home.module.scss';
 
 const questionsAnswers = [
@@ -41,16 +42,29 @@ export default function Home() {
       <Head>
         <title>Senai Parceiros</title>
       </Head>
-      <main className={styles.contentContainer}>
+      <div className={styles.contentContainer}>
         <section className={styles.hero}>
+          <br />
           <h1 className={styles.title}>Mais Parceiros</h1>
-          <p>O programa <strong>Mais Parceiros</strong> foi criado com o intuito de estabelecer um canal direto de comunicação entre o <strong>SENAI</strong> e seus futuros parceiros, apresentar as diretrizes e modelos para consecução de novas parcerias, bem como demostrar os benefícios em ser um parceiro deste programa.</p>
-          <br/>
+          <p>O programa <strong>Mais Parceiros</strong> foi criado com o intuito de estabelecer um canal direto de comunicação entre o 
+           <strong>SENAI</strong> e seus futuros parceiros, apresentar as diretrizes e modelos para consecução de novas parcerias, bem 
+          como demostrar os benefícios em ser um parceiro deste programa.</p>
           <p>O programa estabelece os seguintes benefícios:</p>
-          <br/>
           <Accordion questionsAnswers={questionsAnswers} />
         </section>
-      </main>
+      </div>
+      <div className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <h1 className={styles.title}>Depoimentos de quem já fez parte</h1>
+          <LinkYoutube />
+      </section>
+      </div>
+      <section className={styles.contentContainer}>
+        <h1 className={styles.title}>Modelos de Paceria</h1>
+      </section>
+      <section className={styles.contentContainer}>
+        <h1 className={styles.title}>Como fazer parte</h1>
+      </section>
       <Footer />
     </>
   )
---
+++ src/components/LinkYoutube/index.tsx
@@ -0,0 +1,35 @@
+﻿import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
+import styles from './styles.module.scss';
+
+const slideVideos = [
+    'https://www.youtube.com/embed/wDflrSMTf_Q',
+    'https://youtu.be/mzQZMOPcasQ',
+    'https://youtu.be/3DbnSUSTYXw'
+];
+
+export function LinkYoutube() {
+    return (
+        <>
+            <div>
+                <iframe 
+                    className={styles.video}
+                    src="https://www.youtube.com/embed/wDflrSMTf_Q"
+                    title="+Parceiros – Cisco – Depoimento de quem já faz parte da nossa rede | Senai São Paulo"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                >
+                </iframe>
+                <br /> 
+                <iframe 
+                    className={styles.video}
+                    src="https://www.youtube.com/embed/3DbnSUSTYXw"
+                    title="+Parceiros – Cisco – Depoimento de quem já faz parte da nossa rede | Senai São Paulo"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>                
+            </div>
+        </>
+    )
+}
