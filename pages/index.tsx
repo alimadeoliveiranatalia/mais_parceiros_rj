24cef2999b1c63eebdd3f8fb14d8ae6cf8d089f9
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-10-21 15:55:28 -0300

    feat: font style done

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,38 +5,6 @@ import { LinkYoutube } from '../src/components/LinkYoutube';
 import { SliderShow } from '../src/components/Slidershow';
 import styles from '../styles/Home.module.scss';
 
-const questionsAnswers = [
-  {
-    question: 'Desenvolvimento de Capital Humano',
-    answer: 'As parcerias possibilitam a capacitação do capital humano interno e externo, mantendo atualizados os colaboradores e clientes das empresas parceiras envolvidos no programa.'
-  },
-  {
-    question: 'Atendimento às novas demandas da Indústria',
-    answer: 'Com a inserção de novas tecnologias, torna-se possível a construção e a oferta de produtos e serviços educacionais, tecnológicos e de inovação, compatíveis com a demanda da indústria.'
-  },
-  {
-    question: 'Antecipação de Tendências Tecnológicas',
-    answer: 'É importante que a sociedade esteja preparada para as novas demandas do mercado de trabalho, principalmente quando se trata das tecnologias de vanguarda e habilhitadoras para a indústria 4.0.'
-  },
-  {
-    question: 'Troca de Experiência e Conhecimento',
-    answer: 'Uma parceria pode proporcionar uma ampla troca de experiências, trazer insights, adquirir novas habilhidades, que possam contribuir para a expansão do negócio.'
-  },
-  {
-    question: 'Mais valor agregado para as marcas parceiras',
-    answer: 'Bons parceiros contribuem para elevar o nível de exposição das marcas, garantindo mais visibilidade junto aos clientes.'
-  },
-  {
-    question: 'Difusão Tecnológica',
-    answer: 'Os esforços conjuntos do <strong>SENAI</strong> e de seus parceiros, visam a mordenização de processos, mediante ao uso de produtos e serviços difundidos no mercado.'
-  },
-  {
-    question: 'Capilaridade da Rede Senai',
-    answer: 'Uma parceria consolidada com uma Unidade Operacional do <strong>SENAI</strong> posibilita sua disseminação pela rede no estado, trazendo mais vantagens para as empresas parceiras.'
-  },
-  
-]
-
 export default function Home() {
   return (
     <>
@@ -52,7 +20,6 @@ export default function Home() {
           como demostrar os benefícios em ser um parceiro deste programa.</p>
           <p>O programa estabelece os seguintes benefícios:</p>
           <SliderShow />
-          <Accordion questionsAnswers={questionsAnswers} />
         </section>
       </div>
       <div className={styles.contentContainer}>
--- src/components/Slidershow/index.tsx
+++ src/components/Slidershow/index.tsx
@@ -1,45 +1,62 @@
-﻿import { motion } from 'framer-motion';
+﻿import { motion, useScroll } from 'framer-motion';
 import styles from './styles.module.scss';
 import { useEffect, useRef, useState } from "react";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 const questionsAnswers = [
-    {
-      question: 'Desenvolvimento de Capital Humano',
-      answer: 'As parcerias possibilitam a capacitação do capital humano interno e externo, mantendo atualizados os colaboradores e clientes das empresas parceiras envolvidos no programa.'
-    },
-    {
-      question: 'Atendimento às novas demandas da Indústria',
-      answer: 'Com a inserção de novas tecnologias, torna-se possível a construção e a oferta de produtos e serviços educacionais, tecnológicos e de inovação, compatíveis com a demanda da indústria.'
-    },
-    {
-      question: 'Antecipação de Tendências Tecnológicas',
-      answer: 'É importante que a sociedade esteja preparada para as novas demandas do mercado de trabalho, principalmente quando se trata das tecnologias de vanguarda e habilhitadoras para a indústria 4.0.'
-    },
-    {
-      question: 'Troca de Experiência e Conhecimento',
-      answer: 'Uma parceria pode proporcionar uma ampla troca de experiências, trazer insights, adquirir novas habilhidades, que possam contribuir para a expansão do negócio.'
-    },
-    {
-      question: 'Mais valor agregado para as marcas parceiras',
-      answer: 'Bons parceiros contribuem para elevar o nível de exposição das marcas, garantindo mais visibilidade junto aos clientes.'
-    },
-    {
-      question: 'Difusão Tecnológica',
-      answer: 'Os esforços conjuntos do SENAI e de seus parceiros, visam a mordenização de processos, mediante ao uso de produtos e serviços difundidos no mercado.'
-    },
-    {
-      question: 'Capilaridade da Rede Senai',
-      answer: 'Uma parceria consolidada com uma Unidade Operacional do <strong>SENAI</strong> posibilita sua disseminação pela rede no estado, trazendo mais vantagens para as empresas parceiras.'
-    },
-    
+  {
+    question: 'Desenvolvimento de Capital Humano',
+    answer: 'As parcerias possibilitam a capacitação do capital humano interno e externo, mantendo atualizados os colaboradores e clientes das empresas parceiras envolvidos no programa.'
+  },
+  {
+    question: 'Atendimento às novas demandas da Indústria',
+    answer: 'Com a inserção de novas tecnologias, torna-se possível a construção e a oferta de produtos e serviços educacionais, tecnológicos e de inovação, compatíveis com a demanda da indústria.'
+  },
+  {
+    question: 'Antecipação de Tendências Tecnológicas',
+    answer: 'É importante que a sociedade esteja preparada para as novas demandas do mercado de trabalho, principalmente quando se trata das tecnologias de vanguarda e habilhitadoras para a indústria 4.0.'
+  },
+  {
+    question: 'Troca de Experiência e Conhecimento',
+    answer: 'Uma parceria pode proporcionar uma ampla troca de experiências, trazer insights, adquirir novas habilhidades, que possam contribuir para a expansão do negócio.'
+  },
+  {
+    question: 'Mais valor agregado para as marcas parceiras',
+    answer: 'Bons parceiros contribuem para elevar o nível de exposição das marcas, garantindo mais visibilidade junto aos clientes.'
+  },
+  {
+    question: 'Difusão Tecnológica',
+    answer: 'Os esforços conjuntos do SENAI e de seus parceiros, visam a mordenização de processos, mediante ao uso de produtos e serviços difundidos no mercado.'
+  },
+  {
+    question: 'Capilaridade da Rede Senai',
+    answer: 'Uma parceria consolidada com uma Unidade Operacional do SENAI posibilita sua disseminação pela rede no estado, trazendo mais vantagens para as empresas parceiras.'
+  }    
 ];
 
 
 
 export function SliderShow(){
     const carousel = useRef(null);
+    //const { scrollX } = useScroll({ container: carouselRef });
     const [ widthSlider, setWidthSlider ] = useState(0);
+    const [index, setIndex] = useState(0);
+
+    function nextStep(){
+      if(index === questionsAnswers.length -1){
+        setIndex(0);
+        return
+      }
+      setIndex(index+1);
+    }
+
+    function prevStep(){
+      if(index === 0){
+        setIndex(questionsAnswers.length -1);
+        return
+      }
+      setIndex(index-1);
+    }
 
     useEffect(() => {
         setWidthSlider(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
@@ -48,30 +65,30 @@ export function SliderShow(){
     return(
         <div className={styles.slider}>
             <motion.div
-                ref={ carousel }
-                className={styles.carousel}
-                whileTap={{ cursor: 'grabbing'}}
+              key={index}
+              ref={ carousel }
+              className={styles.carousel}
+              whileTap={{ cursor: 'grabbing'}}
             >
               <AiOutlineLeft />
               <motion.div
-                  className={styles.inner}
-                  drag='x'
-                  dragConstraints={{ right: 0, left: -widthSlider }}
-                  initial={{ x:100 }}
-                  animate={{ x:0 }}
-                  transition={{ duration: 0.8 }}
+                className={styles.inner}
+                drag='x'
+                dragConstraints={{ right: 0, left: -widthSlider }}
+                initial={{ x:0 }}
+                animate={{ x:-100 }}
+                transition={{ type: 'spring'}}
               >
-                  {questionsAnswers.map(item => (
-                      <motion.div
-                          
-                          className={styles.item}    
-                      >
-                          <div><span>{item.question}</span>{item.answer}</div>
-                      </motion.div>
-                  ))}
+                {questionsAnswers.map(item => (
+                  <motion.div                          
+                    className={styles.item}
+                  >
+                    <div><span>{item.question}</span><p>{item.answer}</p></div>
+                  </motion.div>
+                ))}
               </motion.div>
               <AiOutlineRight />
             </motion.div>
-        </div>
+        </div>       
     )
 }
