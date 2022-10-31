7b543b04689f9e296b2e204605f2d8e12e26b7e2
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-10-31 16:57:06 -0300

    refactor: slider videos in component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head';
-import { Accordion } from '../src/components/Accordion';
+import { CarouselVideo } from '../src/components/CarouselVideo';
 import { Footer } from '../src/components/Footer';
-import { LinkYoutube } from '../src/components/LinkYoutube';
-import { SliderCarousel } from '../src/components/Slidershow';
+import { SliderCarousel } from '../src/components/SliderCarousel';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
@@ -14,7 +13,7 @@ export default function Home() {
       <div className={styles.contentContainer}>
         <section className={styles.hero}>
           <br />
-          <h1 className={styles.title}>Mais Parceiros</h1>
+          <h1>Mais Parceiros</h1>
           <p>O programa <strong>Mais Parceiros</strong> foi criado com o intuito de estabelecer um canal direto de comunicação entre o 
           <strong> SENAI</strong> e seus futuros parceiros, apresentar as diretrizes e modelos para consecução de novas parcerias, bem 
           como demostrar os benefícios em ser um parceiro deste programa.</p>
@@ -23,17 +22,25 @@ export default function Home() {
         </section>
       </div>
       <div className={styles.contentContainer}>
-      <section className={styles.hero}>
-        <h1 className={styles.title}>Depoimentos de quem já fez parte</h1>
-          <LinkYoutube />
-      </section>
+        <section className={styles.hero}>
+          <br />
+          <h1>Depoimentos</h1>
+          <p>Conheça o pragrama <strong>Mais Parceiros</strong> pelas palavras de quem já faz parte da nossa rede.</p>
+          <CarouselVideo />
+        </section>
       </div>
-      <section className={styles.contentContainer}>
-        <h1 className={styles.title}>Modelos de Paceria</h1>
-      </section>
-      <section className={styles.contentContainer}>
-        <h1 className={styles.title}>Como fazer parte</h1>
-      </section>
+      <div className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <h1>Modelos de Paceria</h1>
+          <p>O <strong>SENAI-SP</strong> estabelece suas parcerias por meio de modelos de negócios que estabelecem contrapartidas, visando</p>
+        </section>
+      </div>
+      <div className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <h1>Como fazer parte</h1>
+          <p>Caso sua empresa esteja interessada em se tornar uma parceria do <strong>SENAI-SP</strong></p>
+        </section>
+      </div>
       <Footer />
     </>
   )
