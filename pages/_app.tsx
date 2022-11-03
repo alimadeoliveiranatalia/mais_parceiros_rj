a9e64ea97050335b838cf396b8b3931de4659ed0
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-11-03 13:24:05 -0300

    feat: slide functionality done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     axeAccessibilityReporter();
   },[]);
+  
   return (
     <>
       <Header />
--- src/components/SliderShowVideo/index.tsx
+++ src/components/SliderShowVideo/index.tsx
@@ -1,5 +1,5 @@
 ﻿import { AnimatePresence, motion } from "framer-motion";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 import styles from './styles.module.scss';
@@ -14,53 +14,92 @@ const linkVideo = [
         src : 'https://www.youtube.com/embed/3DbnSUSTYXw',
         title: '+Parceiros – Cisco – Depoimento de quem já faz parte da nossa rede | Senai São Paulo',
         company: 'Cisco' 
+    },
+    {
+        src: 'https://youtu.be/mzQZMOPcasQ',
+        title: '+Parceiros – Nokia – Depoimento de quem já faz parte da nossa rede | Senai São Paulo',
+        company: 'Nokia'
+    },
+    {
+        src: 'https://youtu.be/yy7_1qXX3j8',
+        title: '+Parceiros – BMW – Depoimento de quem já faz parte da nossa rede | Senai São Paulo',
+        company: 'BMW Automotiva'
+    },
+    {
+        src: 'https://youtu.be/7NlotjXvcIE',
+        title: '+Parceiros – Triumph – Depoimento de quem já faz parte da nossa rede | Senai São Paulo',
+        company: 'Triumph'
     }
 ];
 
+const variants = {
+    initial: (direction: number) => {
+        return { x: direction > 0 ? 1000 : -1000, opacity: 0 }
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: { 
+            x : { type: 'spring', stiffness: 300, damping: 30 }, 
+            opacity: { duration: 0.2 }
+        }
+    },
+    exit: (direction: number) => {
+        return { 
+            x: direction > 0 ? -1000 : 1000,
+            opacity: 0,
+            transition: {
+                x : { type: 'spring', stiffness: 300, damping: 30},
+                opacity: { duration: 0.2 }
+            } 
+        }
+    }
+}
+
 export function SliderShowVideo(){
-    const slider = useRef(null);
-    const [move, setMove] = useState(false);
+    const [index, setIndex] = useState(0);
+    const [direction, setDirection] = useState(0);
 
-    let passSlider = slider.current;
-
-    function nextSlide(){
-        let width = passSlider.clientWidth;
-        passSlider.scrollLeft = passSlider.scrollLeft + width;
-        console.log(width);
+    function nextVideo(){
+        setDirection(1);
+        if(index === linkVideo.length -1){
+            setIndex(0);
+            return
+        }
+        setIndex(index +1);
     }
 
-    function prevSlide(){
-        let width = passSlider.clientWidth;
-        passSlider.scrollLeft = passSlider.scrollLeft - width;
-        console.log(width);
+    function prevVideo(){
+        setDirection(-1);
+        if(index === 0){
+            setIndex(linkVideo.length -1);
+            return
+        }
+        setIndex(index-1);
     }
-
     return (          
         <div className={styles.slider_video}>           
-            <motion.div
-                ref={ slider }
+            <div
                 className={styles.slider}
-                whileTap={{ cursor: 'grabbing' }}
-            >  
-            { linkVideo.map(item => (
-                <motion.div key={item.src}>
-                    <motion.iframe                        
-                        src={item.src}
-                        title={item.title}
-                        initial={{x:100}}
-                        animate={{x:move ?100:-100, opacity: 3}}
-                        transition={{type:'spring', duration:5}}
+            >
+                <AnimatePresence initial={false} custom={direction}>
+                    <motion.iframe
+                        key={index}
+                        custom={direction}
+                        variants={variants}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        src={linkVideo[index].src}
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
-                    >
-                    </motion.iframe>
-                    <span>{item.company}</span>
-                </motion.div>
-            ))}                
-            </motion.div>
-            <AiOutlineLeft className={styles.icon} onClick={prevSlide}/>            
-            <AiOutlineRight className={styles.icon} onClick={() => {console.log(move);setMove(!move)}}/>
+                    />
+                    <span>{linkVideo[index].company}</span> 
+                </AnimatePresence>            
+            </div>
+            <button onClick={prevVideo}><AiOutlineLeft className={styles.icon}/></button>            
+            <button onClick={nextVideo}><AiOutlineRight className={styles.icon} /></button>
         </div>
     )
 }
