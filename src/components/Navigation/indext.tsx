ed9094b0ac869d02a705bddc5e4a7ccc92356c78
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-11-09 09:21:07 -0300

    refactor: remove link

--- src/components/Navigation/indext.tsx
+++ src/components/Navigation/indext.tsx
@@ -3,8 +3,7 @@
 export function Navigation(){
     return (
         <div className={styles.navigation}>
-            <a href="">Seja Uma Empresa Parceira</a>
-            <a href="">Depoimento de quem já fez parte</a>
+            <a href="">Depoimentos de quem já fez parte</a>
             <a href="">Acesse o Edital para Parcerias</a>
             <a href="">Saiba como fazer parte</a>
         </div>
