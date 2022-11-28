9639796e0fa7114f1f646ab013619dcca31856b1
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-11-28 11:22:40 -0300

    feat: add plugin storybook-addon-sass-postcss

---
+++ .storybook/preview.js
@@ -0,0 +1,9 @@
+﻿export const parameters = {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  }
