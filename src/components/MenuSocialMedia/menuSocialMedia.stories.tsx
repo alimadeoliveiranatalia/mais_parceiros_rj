6c0e076d83eda855dc40c6ecdbe90d98657b91e2
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-11-29 11:18:17 -0300

    feat: add stories components: Header menuSocial

--- src/components/MenuSocialMedia/menuSocialMedia.stories.tsx
+++ src/components/MenuSocialMedia/menuSocialMedia.stories.tsx
@@ -1,9 +1,9 @@
 ﻿import { Meta, StoryObj} from '@storybook/react';
-import { Header } from './';
+import { MenuSocialMedia } from './';
 
 export default {
-    component: Header,
-    title: 'components/Header',
+    component: MenuSocialMedia,
+    title: 'components/MenuSocialMedia'
 } as Meta;
 
 export const Default: StoryObj = {}
