8efdbd8b9289ba19488656e127b15251911758e1
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-11-30 10:13:50 -0300

    feat: add stories in Carousel componet

---
+++ src/components/Carousel/carousel.stories.tsx
@@ -0,0 +1,8 @@
+﻿import { Meta, StoryObj } from "@storybook/react";
+import { Carousel } from "./";
+export default {
+    component: Carousel,
+    title: "components/Carousel"
+} as Meta;
+
+export const Default: StoryObj = {}
