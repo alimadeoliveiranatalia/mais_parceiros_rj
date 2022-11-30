733f1a3787d77f8faadc6a29f6214317bc0e030f
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-11-30 10:07:57 -0300

    feat: add stories in Form componet

---
+++ src/components/Form/form.stories.tsx
@@ -0,0 +1,7 @@
+﻿import { Meta, StoryObj } from "@storybook/react";
+import { Form } from "./";
+export default {
+    component: Form,
+    title: "components/Form"
+} as Meta;
+export const Default : StoryObj = {}
