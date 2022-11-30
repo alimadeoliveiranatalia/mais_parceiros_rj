c28aa5356c1754f75045529db817a299efdf2ad4
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-11-30 10:05:24 -0300

    feat: add stories in MenuToggle componet

---
+++ src/components/MenuToggle/menuToggle.stories.tsx
@@ -0,0 +1,8 @@
+﻿import { Meta, StoryObj } from "@storybook/react";
+import { MenuToggle } from "./";
+
+export default {
+    component: MenuToggle,
+    title: "components/MenuToggle"
+} as Meta;
+export const Default : StoryObj = {}
