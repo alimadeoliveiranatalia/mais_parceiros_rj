a516a485a0bc4eff65d708525aa7c3034b40e222
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-11-30 10:11:06 -0300

    feat: add stories in Footer componet

---
+++ src/components/Footer/footer.stories.tsx
@@ -0,0 +1,9 @@
+﻿import { Meta, StoryObj } from "@storybook/react";
+import { Footer } from "./";
+
+export default {
+    component: Footer,
+    title: "components/Footer"
+} as Meta;
+
+export const Default: StoryObj = {}
