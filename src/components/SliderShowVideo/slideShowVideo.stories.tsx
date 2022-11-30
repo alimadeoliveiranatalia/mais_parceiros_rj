2b011c7561366e5800b290bef5b6507f6a04272b
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-11-30 10:01:42 -0300

    feat: add stories in slidershowvideo componet

---
+++ src/components/SliderShowVideo/slideShowVideo.stories.tsx
@@ -0,0 +1,9 @@
+﻿import { Meta, StoryObj } from "@storybook/react";
+import { SliderShowVideo } from "./";
+
+export default {
+    component: SliderShowVideo,
+    title: "components/SliderShowVideo"
+} as Meta
+
+export const Default: StoryObj = {}
