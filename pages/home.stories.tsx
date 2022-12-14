c360679d6a15c9e2522b0e9226313607107f65bf
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2022-12-14 11:39:55 -0300

    feat: add story home

---
+++ pages/home.stories.tsx
@@ -0,0 +1,16 @@
+﻿import { StoryObj } from "@storybook/react";
+import  Home from "./index";
+import { Carousel } from "../src/components/Carousel";
+import { Form } from "../src/components/Form";
+import { Header } from "../src/components/Header";
+import { MenuSocialMedia } from "../src/components/MenuSocialMedia";
+import { Navigation } from "../src/components/Navigation";
+import { SliderShowVideo } from "../src/components/SliderShowVideo";
+
+export default {
+    component: Home,
+    subcomponents: [ Header, MenuSocialMedia, Navigation, SliderShowVideo, Carousel, Form],
+    title: "Pages/Home"
+} 
+
+export const Default: StoryObj = {}
