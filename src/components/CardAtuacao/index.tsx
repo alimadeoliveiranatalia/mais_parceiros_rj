fed29bd2fc7c120038baabe383441e2205b7878f
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-01-03 11:50:18 -0300

    refactor: remove import

--- src/components/CardAtuacao/index.tsx
+++ src/components/CardAtuacao/index.tsx
@@ -1,9 +1,4 @@
-﻿import Image from "next/image";
-import { useState } from "react";
-import { RiPlantFill } from "react-icons/ri";
-import parceiros from "../../../public/parceiros.json";
-import { ContentModal } from "../ContentModal";
-
+﻿
 import styles from "./styles.module.scss";
 
 export function CardAtuacao({title}){
