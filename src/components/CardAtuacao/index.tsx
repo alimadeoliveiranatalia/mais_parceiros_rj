6103259f7eda7f9a7b5e106139edb12821933a1b
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-01-03 16:43:50 -0300

    fix: verify modal

--- src/components/CardAtuacao/index.tsx
+++ src/components/CardAtuacao/index.tsx
@@ -1,13 +1,17 @@
 ﻿import styles from "./styles.module.scss";
 import Image from "next/image";
+import { Modal } from "../Modal";
 
 export function CardAtuacao({title, src}){
     
     return (        
-        <button className={styles.card} >
-            <Image src={src} alt='image' width={40} height={40} />
-            <p>{title}</p>
-        </button>        
+        <div className={styles.container}>
+            <button className={styles.card} >
+                <Image src={src} alt='image' width={40} height={40} />
+                <p>{title}</p>
+            </button>
+            <Modal />
+        </div>        
     )
 }
 
--- src/components/ContentModal/index.tsx
+++ src/components/ContentModal/index.tsx
@@ -1,25 +1,29 @@
 ﻿import Image from "next/image";
 import logo from "../../../public/icons_parceiros/john_deere.svg";
+import { HeaderModal } from "../HeaderModal";
 import styles from "./styles.module.scss";
 
 export function ContentModal({ empresa, escola }){
     return (
-        <table className={styles.table}>
-            <tr>
-                <th></th>
-                <th style={{'textAlign': 'left'}}>Empresa</th>
-                <th style={{'textAlign': 'left'}}>Escola Responsável</th>
-            </tr>
-            <tr>
-                <td style={{'textAlign':'center'}}><Image src={logo} alt='logo empresa' width={45} height={45}/></td>
-                <td>John Deere do Brasil</td>
-                <td>602 - Escola Senai - Ribeirão Preto - Eng. </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>{empresa}</td>
-                <td>{escola}</td>
-            </tr>
-        </table>
+        <>
+            <HeaderModal />
+            <table className={styles.table}>
+                <tr>
+                    <th></th>
+                    <th style={{'textAlign': 'left'}}>Empresa</th>
+                    <th style={{'textAlign': 'left'}}>Escola Responsável</th>
+                </tr>
+                <tr>
+                    <td style={{'textAlign':'center'}}><Image src={logo} alt='logo empresa' width={45} height={45}/></td>
+                    <td>John Deere do Brasil</td>
+                    <td>602 - Escola Senai - Ribeirão Preto - Eng. </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>{empresa}</td>
+                    <td>{escola}</td>
+                </tr>
+            </table>
+        </>
     )
 }
