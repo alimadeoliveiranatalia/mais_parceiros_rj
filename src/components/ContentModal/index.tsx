5911400bdc3abd9ab85674da58c30137610eb80b
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-01-04 11:47:32 -0300

    refactor: modal component done

--- src/components/ContentModal/index.tsx
+++ src/components/ContentModal/index.tsx
@@ -11,28 +11,26 @@ export interface ModalProps{
 
 export function ContentModal({ area, empresa, escola }: ModalProps){
     return (
-        <>
+        <div>
             <div className={styles.headermodal}>
-                <p>{area}</p>
+                <span>{area}</span>
                 <AiFillCloseCircle className={styles.iconClose}/>
             </div>
             <table className={styles.table}>
                 <tr>
-                    <th></th>
-                    <th style={{'textAlign': 'left'}}>Empresa</th>
+                    <th>Empresa</th>
+                    <th style={{'textAlign': 'left'}}></th>
                     <th style={{'textAlign': 'left'}}>Escola Responsável</th>
                 </tr>
+                <br />
                 <tr>
-                    <td style={{'textAlign':'center'}}><Image src={logo} alt='logo empresa' width={45} height={45}/></td>
-                    <td>John Deere do Brasil</td>
-                    <td>602 - Escola Senai - Ribeirão Preto - Eng. </td>
-                </tr>
-                <tr>
-                    <td></td>
+                    <td style={{'textAlign':'center'}}>
+                        <Image src={logo} alt='logo empresa' width={45} height={45}/>
+                    </td>
                     <td>{empresa}</td>
                     <td>{escola}</td>
                 </tr>
             </table>
-        </>
+        </div>
     )
 }
