958165e0c5d99f3b1e27017d864811896f63bf66
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-01-06 11:00:03 -0300

    refactor:render component modal

--- src/components/ContentModal/index.tsx
+++ src/components/ContentModal/index.tsx
@@ -16,24 +16,26 @@ export function ContentModal({ area, empresa, escola }: ModalProps){
                 <span>{area}</span>
                 <AiFillCloseCircle className={styles.iconClose}/>
             </div>
-            <table className={styles.table}>
-                <thead>
-                    <tr>
-                        <th>Empresa</th>
-                        <th style={{'textAlign': 'left'}}></th>
-                        <th style={{'textAlign': 'left'}}>Escola Responsável</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td style={{'textAlign':'center'}}>
-                            <Image src={logo} alt='logo empresa' width={45} height={45}/>
-                        </td>
-                        <td>{empresa}</td>
-                        <td>{escola}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+                            <th>Empresa</th>
+                            <th style={{'textAlign': 'left'}}></th>
+                            <th style={{'textAlign': 'left'}}>Escola Responsável</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style={{'textAlign':'center'}}>
+                                <Image src={logo} alt='logo empresa' width={45} height={45}/>
+                            </td>
+                            <td>{empresa}</td>
+                            <td>{escola}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
