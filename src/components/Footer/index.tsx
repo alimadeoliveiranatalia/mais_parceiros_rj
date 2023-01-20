152c375ac01a5afc6d790c098ea36b00dedabff4
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-01-20 14:35:07 -0300

    refactor: footer in web mobile

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,5 @@
 ﻿import {ImLocation2, ImPhone} from 'react-icons/im';
+import { MenuSocialMedia } from '../MenuSocialMedia';
 import styles from './styles.module.scss';
 
 export function Footer(){
@@ -6,15 +7,15 @@ export function Footer(){
         <footer role='contentinfo' className={styles.footerConteiner}>
             <div className={styles.footerContent}>
                 <div>
-                    <span> 
-                        <ImLocation2 />
+                    <ImLocation2 className={styles.icon}/>
+                    <span>                        
                         Rua de Pedestre SE
                     </span>
                     <p>Av. Paulista</p>
                 </div>
                 <div>
+                    <ImPhone className={styles.icon}/>
                     <span>
-                        <ImPhone />
                         Central de Atendimento
                     </span>
                     <p>(11)088-055-1000</p>
