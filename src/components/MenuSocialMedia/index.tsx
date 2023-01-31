eba413d2ccfe655c6f220b433570e401c7b64b75
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-01-31 13:51:58 -0300

    refactor: add urls in links

--- src/components/MenuSocialMedia/index.tsx
+++ src/components/MenuSocialMedia/index.tsx
@@ -1,14 +1,14 @@
-﻿import { ImFacebook, ImTwitter, ImYoutube, ImInstagram } from 'react-icons/im';
+﻿import { ImFacebook, ImTwitter, ImYoutube, ImLinkedin } from 'react-icons/im';
 
 import styles from './styles.module.scss';
 
 export function MenuSocialMedia(){
     return (
         <div className={styles.navConteiner}>
-            <a aria-label='Perfil no Facebook' href="#" ><ImFacebook /></a>
-            <a aria-label='Página do Twitter' href="#" ><ImTwitter /></a>
-            <a aria-label='Canal no Youtube' href="#" ><ImYoutube /></a>
-            <a aria-label='Perfil no Instagram' href="#" ><ImInstagram /></a>
+            <a aria-label='Perfil no Facebook' href="https://web.facebook.com/firjanoficial?_rdc=1&_rdr" ><ImFacebook /></a>
+            <a aria-label='Página do Twitter' href="https://twitter.com/firjan" ><ImTwitter /></a>
+            <a aria-label='Canal no Youtube' href="https://www.youtube.com/user/SistemaFIRJAN" ><ImYoutube /></a>
+            <a aria-label='Perfil no Instagram' href="https://www.linkedin.com/company/firjan/" ><ImLinkedin /></a>
         </div>
     )
 }
