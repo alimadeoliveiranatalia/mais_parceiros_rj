15a8aa7867b84d80f8d9a0dc615baa0279c80ea5
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-01-31 09:52:24 -0300

    refactor: clear components

--- src/components/MenuToggle/menuToggle.stories.tsx
+++
@@ -1,8 +0,0 @@
-﻿import { Meta, StoryObj } from "@storybook/react";
-import { MenuToggle } from "./";
-
-export default {
-    component: MenuToggle,
-    title: "components/MenuToggle"
-} as Meta;
-export const Default : StoryObj = {}
