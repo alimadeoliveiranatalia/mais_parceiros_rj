6103a63606c0f98a017aba162d9dbc3d8428ad80
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-02-07 09:56:49 -0300

    refactor: fazendo build

---
+++ out/_next/static/6Ml7M2zpJ5H-bKKrn7RdB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/922-9c29e4ec56129d28.js","static/css/1af89bf3a2dec821.css","static/chunks/pages/index-0aea89245a02bead.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
