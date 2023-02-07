5d8fd594799dd2cd5a6c73a32e2d3b499ea83ec5
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-02-07 13:37:58 -0300

    refactor: add default availables

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assertPrefix = "";
+
+let basePath = "/";
+
+if(isGithubActions){
+ const repositorio = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+ assertPrefix = `/${repositorio}/`;
+
+ basePath = `/${repositorio}/`;
+}
+
 const nextConfig = {
-  basePath: "/",
-  assetPrefix: "/",
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true
-  }
+  assetPrefix: assertPrefix,
+  basePath: basePath,
+  
 }
 
 module.exports = nextConfig
