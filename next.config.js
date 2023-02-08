806b312654cb6e889653d8eaa4baf7b3e074eac1
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-02-08 16:58:55 -0300

    refactor: add assertPrefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = (process.env.NODE_ENV || "production") === "production";
+
+const assertPrefix = isProd ? "/mais_parceiros_rj" : "";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -7,6 +11,10 @@ const nextConfig = {
   },
   /*basePath: "/mais_parceiros_rj",
   assetPrefix: "/mais_parceiros_rj"*/
+  exportPathMap: () => ({
+    "/": { page: "/"}
+  }),
+  assertPrefix: assertPrefix,
 }
 
 module.exports = nextConfig;
