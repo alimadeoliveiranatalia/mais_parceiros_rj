12ac25677234bcb04a02bb04ac51ffe935921706
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-02-10 09:36:51 -0300

    refactor: add basePath and assertPrefix

--- next.config.js
+++ next.config.js
@@ -1,20 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = (process.env.NODE_ENV || "production") === "production";
-
-const assertPrefix = isProd ? "/mais_parceiros_rj" : "";
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true
   },
-  /*basePath: "/mais_parceiros_rj",
-  assetPrefix: "/mais_parceiros_rj"*/
-  exportPathMap: () => ({
-    "/": { page: "/"}
-  }),
-  assertPrefix: assertPrefix,
+  basePath: "/mais_parceiros_rj",
+  assetPrefix: "/mais_parceiros_rj/"
 }
 
 module.exports = nextConfig;
